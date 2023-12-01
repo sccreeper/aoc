d284d0f7d60ec198c9b8819c1dc3eb1e7a2e4d64
Oscar Peace <oscar@oscarcp.net>
2023-12-01 18:16:08 +0000

    Make 2023.1 faster

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -1,37 +1,33 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
-	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const integers = "1234567890"
 
 var words []string = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+var word_lengths []int = []int{3, 3, 5, 4, 4, 3, 5, 5, 4}
 var word_digits []string = []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+
+//go:embed calibration.txt
+var input_string string
+var total int
 
 func main() {
 
-	var input_name string
-	fmt.Println("Input file name:")
-	fmt.Scanln(&input_name)
-
-	input_data, err := os.ReadFile(input_name)
-	if err != nil {
-		fmt.Printf("No input called: %s", input_name)
-	}
-
-	input_string := string(input_data)
-
-	var total int
+	start := time.Now()
 
 	for _, line := range strings.Split(input_string, "\n") {
 
 		var line_numbers []string = make([]string, 0)
+		var line_length int = len(line)
 
-		for i := 0; i < len(line); i++ {
+		for i := 0; i < line_length; i++ {
 
 			if strings.Contains(integers, string(line[i])) { //Words can't start with integers.
 
@@ -39,11 +35,11 @@ func main() {
 
 			} else {
 
-				for j := 0; j < len(words); j++ {
+				for j := 0; j < 9; j++ {
 
-					if i+len(words[j]) < len(line)+1 {
+					if i+word_lengths[j] < line_length+1 {
 
-						if string(line[i:i+len(words[j])]) == words[j] {
+						if string(line[i:i+word_lengths[j]]) == words[j] {
 
 							line_numbers = append(line_numbers, word_digits[j])
 
@@ -63,17 +59,18 @@ func main() {
 
 			i, _ := strconv.Atoi(line_numbers[0] + line_numbers[0])
 			total += i
-			fmt.Println(i)
 
 		} else {
 
 			i, _ := strconv.Atoi(line_numbers[0] + line_numbers[len(line_numbers)-1])
 			total += i
-			fmt.Println(i)
 
 		}
 
 	}
+
+	used := time.Since(start)
+	fmt.Println("Time used:", used)
 
 	fmt.Println(total)
 
