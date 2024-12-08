585161026dcec73c5d34eb43e6d0aa2b2bb3afe5
Oscar Peace <oscar@oscarcp.net>
2024-12-08 13:36:51 +0000

    part: 2024.8.1

---
+++ 2024/8/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type Node struct {
+	X    int
+	Y    int
+	Type byte
+}
+
+func parseData(data []byte) (result map[byte][]Node, width int, height int) {
+
+	lines := make([]string, 0)
+
+	accumulator := ""
+
+	for _, b := range data {
+
+		if b == '\n' {
+
+			lines = append(lines, accumulator)
+			accumulator = ""
+
+		} else {
+			accumulator += string(b)
+		}
+
+	}
+
+	lines = append(lines, accumulator)
+
+	result = make(map[byte][]Node, 0)
+
+	for y := 0; y < len(lines); y++ {
+
+		for x := 0; x < len(lines[0]); x++ {
+
+			if lines[y][x] == '.' {
+				continue
+			}
+
+			if _, exists := result[lines[y][x]]; !exists {
+				result[lines[y][x]] = make([]Node, 0)
+			}
+
+			result[lines[y][x]] = append(
+				result[lines[y][x]],
+				Node{
+					X:    x,
+					Y:    y,
+					Type: lines[y][x],
+				},
+			)
+
+		}
+
+	}
+
+	width = len(lines[0])
+	height = len(lines)
+
+	return
+
+}
+
+func containsPos(positions [][2]int, pos [2]int) (result bool) {
+
+	for _, v := range positions {
+		result = v[0] == pos[0] && v[1] == pos[1]
+		if result {
+			return
+		}
+	}
+
+	return
+
+}
+
+func partOne(nodeMap map[byte][]Node, width int, height int) (total int) {
+
+	antiNodePositions := make([][2]int, 0)
+
+	for _, nodes := range nodeMap {
+
+		for i := 0; i < len(nodes); i++ {
+
+			for j := 0; j < len(nodes); j++ {
+				if nodes[i].X == nodes[j].X && nodes[i].Y == nodes[j].Y {
+					continue
+				}
+
+				diff := [2]int{
+					nodes[i].X - nodes[j].X,
+					nodes[i].Y - nodes[j].Y,
+				}
+
+				antiNodePos := [2]int{
+					nodes[i].X + diff[0],
+					nodes[i].Y + diff[1],
+				}
+
+				if antiNodePos[0] >= 0 && antiNodePos[0] < width && antiNodePos[1] >= 0 && antiNodePos[1] < height {
+					if !containsPos(antiNodePositions, antiNodePos) {
+						antiNodePositions = append(antiNodePositions, antiNodePos)
+					}
+				}
+			}
+
+		}
+
+	}
+
+	fmt.Println(antiNodePositions)
+
+	return len(antiNodePositions)
+
+}
+
+func main() {
+
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	parsed, width, height := parseData(data)
+
+	fmt.Println(partOne(parsed, width, height))
+
+}
