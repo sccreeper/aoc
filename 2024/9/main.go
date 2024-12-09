d646a7c2b425337d950ec64270c763d438f47bfd
Oscar Peace <oscar@oscarcp.net>
2024-12-09 18:34:27 +0000

    attempt: 2024.9.2
    works on test input but not actual input

--- 2024/9/main.go
+++ 2024/9/main.go
@@ -145,6 +145,139 @@ func partOne(diskMap []int, fileCount int) (checksum int) {
 
 }
 
+// Finds the biggest space that will fit a given file
+func findFileSpace(diskMap []int, fileLength int) (spaceIndex int) {
+
+	var inFileSpace bool = false
+	var fileSpaceSize int = 0
+
+	for i := 0; i < len(diskMap); i++ {
+
+		if diskMap[i] == -1 && inFileSpace {
+			fileSpaceSize++
+		} else if diskMap[i] != -1 && inFileSpace {
+
+			inFileSpace = false
+
+			if fileLength <= fileSpaceSize {
+				return i - fileSpaceSize
+			} else {
+				fileSpaceSize = 0
+				continue
+			}
+
+		} else if diskMap[i] == -1 && !inFileSpace {
+			inFileSpace = true
+			fileSpaceSize++
+		}
+	}
+
+	return -1
+
+}
+
+func prettifyDiskMap(diskMap []int) (result string) {
+
+	result = ""
+
+	for _, v := range diskMap {
+
+		if v == -1 {
+			result += "."
+		} else {
+			result += strconv.Itoa(v)
+		}
+
+	}
+
+	return
+
+}
+
+func partTwo(diskMap []int) (checksum int) {
+
+	// Map of file IDs, their leftmost indexes and their length
+	var fileIDattr map[int][2]int = make(map[int][2]int)
+	var fileIDs []int = make([]int, 0)
+
+	var currentFile int = -1
+	var currentFileLength int = 0
+	var inFile bool = false
+
+	for i := len(diskMap) - 1; i >= 0; i-- {
+
+		if inFile {
+			if diskMap[i] != currentFile {
+
+				fileIDattr[currentFile] = [2]int{i + 1, currentFileLength}
+				fileIDs = append(fileIDs, currentFile)
+
+				i++
+				currentFile = -1
+				inFile = false
+				currentFileLength = 0
+			} else {
+				currentFileLength++
+			}
+		} else {
+			if diskMap[i] == -1 {
+				continue
+			} else {
+				currentFile = diskMap[i]
+				currentFileLength = 1
+				inFile = true
+			}
+		}
+
+	}
+
+	// fmt.Println(fileIDattr)
+
+	// Sort file IDs then loop through in descending order
+	slices.Sort(fileIDs)
+
+	for i := len(fileIDs) - 1; i >= 0; i-- {
+		leftMostIndex := fileIDattr[fileIDs[i]][0]
+		fileLength := fileIDattr[fileIDs[i]][1]
+
+		fileIndex := findFileSpace(diskMap[:leftMostIndex], fileLength)
+
+		if fileIndex != -1 {
+
+			fileSource := diskMap[leftMostIndex : leftMostIndex+fileIndex]
+
+			copy(diskMap[fileIndex:fileIndex+fileLength], fileSource)
+			copy(diskMap[leftMostIndex:leftMostIndex+fileLength], blankArray[:fileLength])
+
+			// fmt.Println(prettifyDiskMap(diskMap))
+
+		}
+
+	}
+
+	// Calculate checksum
+
+	var checksumEnd int
+	for i := len(diskMap) - 1; i >= 0; i-- {
+		if diskMap[i] != -1 {
+			checksumEnd = i + 1
+			break
+		}
+	}
+
+	for i := 0; i < checksumEnd; i++ {
+
+		if diskMap[i] == -1 {
+			continue
+		}
+
+		checksum += diskMap[i] * i
+
+	}
+
+	return
+}
+
 func main() {
 	data, err := io.ReadAll(os.Stdin)
 	if err != nil {
@@ -154,9 +287,12 @@ func main() {
 	startTime := time.Now().UnixMicro()
 
 	fmt.Println("Parsing data...")
-	parsed, filesCount := parseData(data)
-	fmt.Println("Part one...")
-	fmt.Printf("%d\n", partOne(parsed, filesCount))
+	// parsed, filesCount := parseData(data)
+	// fmt.Println("Part one...")
+	// fmt.Printf("%d\n", partOne(parsed, filesCount))
+	parsed, _ := parseData(data)
+	fmt.Println("Part two...")
+	fmt.Printf("%d\n", partTwo(parsed))
 
 	fmt.Printf("Finished in %dus\n", time.Now().UnixMicro()-startTime)
 }
