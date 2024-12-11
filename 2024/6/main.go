b4b0fcefdb90b7e42e1876c8c32e67f353880ea0
Oscar Peace <oscar@oscarcp.net>
2024-12-11 23:03:51 +0000

    part: 2024.6.2

--- 2024/6/main.go
+++ 2024/6/main.go
@@ -120,8 +120,102 @@ func checkForLoop() {
 
 }
 
+func containsPosition(visitedPositions [][3]int, pos [3]int) (idx int) {
+
+	for i := len(visitedPositions) - 1; i > 0; i-- {
+
+		if visitedPositions[i][0] == pos[0] && visitedPositions[i][1] == pos[1] && visitedPositions[i][2] == pos[2] {
+			return i
+		}
+
+	}
+
+	return -1
+
+}
+
 func partTwo(mapRows [][]byte, guardPosX int, guardPosY int) (totalLoops int) {
+
+	guardStartX := guardPosX
+	guardStartY := guardPosY
+
+	for y := 0; y < len(mapRows); y++ {
+		for x := 0; x < len(mapRows); x++ {
+
+			guardPosX = guardStartX
+			guardPosY = guardStartY
+
+			if mapRows[y][x] == '#' || (guardPosX == x && guardPosY == y) {
+				continue
+			}
+
+			mapRows[y][x] = obstruction
+
+			var direction guardDirection = north
+			var visitedPositions [][3]int = make([][3]int, 0)
+			var possiblyInLoop bool
+			var possibleLoopLength int
+			var loopStartPos [3]int
+
+			for guardPosX < len(mapRows[0])-1 && guardPosX > 0 && guardPosY < len(mapRows)-1 && guardPosY > 0 {
+
+				contP := containsPosition(visitedPositions, [3]int{guardPosX, guardPosY, int(direction)})
+				fmt.Printf("\r Obstruction XY: %d,%d...", x, y)
+
+				if possiblyInLoop {
+
+					if len(visitedPositions)-contP+1 == possibleLoopLength && loopStartPos[0] == guardPosX && loopStartPos[1] == guardPosY && loopStartPos[2] == int(direction) {
+						totalLoops++
+						break
+					}
+
+				} else if !possiblyInLoop && contP != -1 {
+					possiblyInLoop = true
+					possibleLoopLength = len(visitedPositions) - contP + 1
+					loopStartPos = [3]int{guardPosX, guardPosY, int(direction)}
+				}
+
+				visitedPositions = append(visitedPositions, [3]int{guardPosX, guardPosY, int(direction)})
+
+				// "Clamp" direction
+
+				if direction > west {
+					direction = north
+				}
+
+				var moveDirection [2]int
+
+				switch direction {
+				case north:
+					moveDirection = [2]int{0, -1}
+				case east:
+					moveDirection = [2]int{1, 0}
+				case south:
+					moveDirection = [2]int{0, 1}
+				case west:
+					moveDirection = [2]int{-1, 0}
+				default:
+					panic("wrong direction")
+				}
+
+				if mapRows[guardPosY+moveDirection[1]][guardPosX+moveDirection[0]] == obstruction {
+					direction++
+				} else {
+
+					guardPosX += moveDirection[0]
+					guardPosY += moveDirection[1]
+
+				}
+
+			}
+
+			mapRows[y][x] = emptySpace
+
+		}
+	}
+
 	return
+
 }
 
 func main() {
